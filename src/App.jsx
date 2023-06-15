acb0c8a7bb99e00aad8819b23bcb5ba6a7122801
Alun Price <alunprice089@gmail.com>
2023-06-15 14:09:44 +0100

    519 Initial commit Posts List and Modal comps done MainHeader just added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MainHeader from "./components/MainHeader";
+import PostsList from "./components/PostsList";
+
+function App() {
+  return (
+    <>
+      <MainHeader />
+      <PostsList />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/PostsList.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import Post from "./Post";
+import NewPost from "./NewPost";
+import classes from "./PostsList.module.css";
+import Modal from "./Modal";
+
+function PostsList(props) {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
+  return (
+    <>
+      {modalIsVisible ? (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      ) : (
+        false
+      )}
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Manuel" body="Check out the full course!!!" />
+      </ul>
+    </>
+  );
+}
+
+export default PostsList;
